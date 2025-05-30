46f3f6835e6f2c3731ad43d920e222b85c8b44a3
EC2 Default User <ec2-user@ip-172-31-39-16.ec2.internal>
2025-05-30 20:32:58 +0000

    prvi

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     strictPort: true,
     host: "0.0.0.0",
     allowedHosts: [
-      "projekat2-alb-1872586309.us-east-1.elb.amazonaws.com",
+      "projekat2-alb-878172051.us-east-1.elb.amazonaws.com",
       "localhost",
       "127.0.0.1",
     ],
